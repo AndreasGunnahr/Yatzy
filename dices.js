a3e9b7334f94c11032a824238b0e34d60c23bf1b
Andreas Gunnahr <Andreas.Gunnahr@hotmail.com>
2019-09-12 13:07:15 +0200

    Nuvarande spelaras boarder, cell färger etc

--- dices.js
+++ dices.js
@@ -7,66 +7,101 @@ let playerArray = [];
 let diceValuesArray = [];
 let numbers = [6,5,4,3,2,1];
 let counter = 0;
-let numberOfThrows = 0; 
+let numberOfThrows = 3; 
 let diceValue = 0; 
 let playerIndex = 0;
 let totalSum = 0;
 let startIndex = 0;
 let stash = 0;
+let countPlayers = 0;
 
 // TODO: Kolla om man kan lösa så man kan ha samma av flera namn. Problem när man tar bort ena (index).
 // TODO: Animera tärningarna på kast. 
 
+document.getElementById(tableSpots[0]).innerHTML = "Andreas";
+document.getElementById(tableSpots[0]).value = "Andreas";
+document.getElementById(tableSpots[1]).innerHTML = "Oskar";
+document.getElementById(tableSpots[1]).value = "Oskar";
+document.getElementById(tableSpots[2]).innerHTML = "Camilla";
+document.getElementById(tableSpots[2]).value = "Camilla";
+document.getElementById(tableSpots[3]).innerHTML = "Pettersson";
+document.getElementById(tableSpots[3]).value = "Pettersson";
+playerArray[0] = "Andreas";
+playerArray[1] = "Oskar";
+playerArray[2] = "Camilla";
+playerArray[3] = "Pettersson";
 
+document.getElementById("player0").style.border = "3px solid black";
+addColor(playerIndex);
+/*
+class Dices{
+    constructor(value){
+        this.value = value;
+    }
+}
 
+class Player{
+    constructor(username, password, image){
+        this.username = username; 
+        this.password = password; 
+        this.image = image; 
+    }
+}
+
+class Table{
+    constructor(){
+        this.cellId = cellId;
+        this.
+    }
+} */
 
 // Funktion där vi lägger till varje spelares namn i tabellen. 
-function addPlayerNames(){
-    let playerNameLower = document.getElementById("playerNames").value.toLowerCase();
-    let playerName = playerNameLower.charAt(0).toUpperCase() + playerNameLower.slice(1);
+// function addPlayerNames(){
+//     let playerNameLower = document.getElementById("playerNames").value.toLowerCase();
+//     let playerName = playerNameLower.charAt(0).toUpperCase() + playerNameLower.slice(1);
 
-    //Fixa så att man inte kan skicka in siffror. 
-    if(playerName == ""){
-        alert("Please enter a correct name.")
-    }
-    else if(counter == 4){
-        alert("You have added maximum players (4 players)");
-    }
-    else{
-        if(document.getElementById(tableSpots[counter]).innerHTML == "" || document.getElementById(tableSpots[counter]).innerHTML.value == undefined){
-            document.getElementById(tableSpots[counter]).innerHTML = playerName;
-            document.getElementById(tableSpots[counter]).value = playerName;
-            playerArray[counter] = playerName;
-            counter += 1;
-        }
-    }
-}
+//     //Fixa så att man inte kan skicka in siffror. 
+//     if(playerName == ""){
+//         alert("Please enter a correct name.")
+//     }
+//     else if(counter == 4){
+//         alert("You have added maximum players (4 players)");
+//     }
+//     else{
+//         if(document.getElementById(tableSpots[counter]).innerHTML == "" || document.getElementById(tableSpots[counter]).innerHTML.value == undefined){
+//             document.getElementById(tableSpots[counter]).innerHTML = playerName;
+//             document.getElementById(tableSpots[counter]).value = playerName;
+//             playerArray[counter] = playerName;
+//             counter += 1;
+//         }
+//     }
+// }
 
 // Funktion där vi tar bort en spelares namn från tabellen. 
-function removePlayerNames(){
-    let playerNameLower = document.getElementById("playerNames").value.toLowerCase();
-    let playerName = playerNameLower.charAt(0).toUpperCase() + playerNameLower.slice(1);
+// function removePlayerNames(){
+//     let playerNameLower = document.getElementById("playerNames").value.toLowerCase();
+//     let playerName = playerNameLower.charAt(0).toUpperCase() + playerNameLower.slice(1);
 
-    if(playerName == "" || !playerArray.includes(playerName)){
-        alert("Please enter a already given name.")
-    }
+//     if(playerName == "" || !playerArray.includes(playerName)){
+//         alert("Please enter a already given name.")
+//     }
 
-    else if(playerArray.includes(playerName)){
-        let index = playerArray.indexOf(playerName);
-        if (index > -1) {
-            playerArray.splice(index, 1);
-            document.getElementById(tableSpots[index]).innerHTML = "";
-            document.getElementById(tableSpots[index]).value = "";
-            for(let x = 0; x < 4; x++){
-                document.getElementById(tableSpots[x]).innerHTML = "";
-            }
-            for(let x = 0; x < playerArray.length; x++){
-                document.getElementById(tableSpots[x]).innerHTML = playerArray[x];
-            }
-            counter = playerArray.length;
-        }
-    }
-}
+//     else if(playerArray.includes(playerName)){
+//         let index = playerArray.indexOf(playerName);
+//         if (index > -1) {
+//             playerArray.splice(index, 1);
+//             document.getElementById(tableSpots[index]).innerHTML = "";
+//             document.getElementById(tableSpots[index]).value = "";
+//             for(let x = 0; x < 4; x++){
+//                 document.getElementById(tableSpots[x]).innerHTML = "";
+//             }
+//             for(let x = 0; x < playerArray.length; x++){
+//                 document.getElementById(tableSpots[x]).innerHTML = playerArray[x];
+//             }
+//             counter = playerArray.length;
+//         }
+//     }
+// }
 
 // Funktion som gör att vi kan spara värdena på tärningarna om vi klickar på dem.
 function saveDiceValue(clicked){
@@ -81,19 +116,110 @@ function saveDiceValue(clicked){
 
 // Funktion där vi kollar om max antal kast är gjorda. 
 function checkMaxThrow(){
-    if(numberOfThrows < 3){
-        numberOfThrows += 1;
-        document.getElementById("nrThrows").innerHTML = "You have made: " + numberOfThrows + " " + "throw";
+    if(numberOfThrows > 0 ){
+        numberOfThrows -= 1;
+        document.getElementById("nrThrows").innerHTML = "Number of throws left: " + numberOfThrows;
         for(let x = 0; x < 5; x ++){
-            if(numberOfThrows != 3 && document.getElementById(diceArray[x]).style.backgroundColor != "grey"){
+            if(numberOfThrows != 0 && document.getElementById(diceArray[x]).style.backgroundColor != "grey"){
                 document.getElementById(diceArray[x]).value = Math.floor(Math.random() * 6) + 1;
             }
-            else if(numberOfThrows == 3 ){
+            else if(numberOfThrows == 0 ){
+                document.getElementById(diceArray[x]).value = Math.floor(Math.random() * 6) + 1;
                 document.getElementById(diceArray[x]).style.backgroundColor = "red";
                 document.getElementById(diceArray[x]).disabled = true;
             }   
         }   
     }
+}
+
+// Funktion som tar bort färgen på borders och tagna celler. 
+function removeColor(index){
+    for(let i = 0; i < 19; i++){
+        for(let j=index+1; j < index+2; j++){
+           let tableCell =  document.getElementById("gameBoard").rows[i].cells[j];
+        //    console.log(tab);
+           if(i == 0){
+               tableCell.style.borderTop = "2px solid black";
+           }
+           else if(i == 18){
+                tableCell.style.borderBottom = "2px solid black";
+           }
+           if(i != 0 && i != 7 && i != 8 && i != 18){
+            tableCell.style.backgroundColor = "white";
+           }
+                tableCell.style.borderRight = "2px solid black";
+                tableCell.style.borderLeft = "2px solid black"
+        }
+    }
+}
+
+// Funktion som lägger till färg på borders och lediga celler. 
+function addColor(index){      
+    for(let i = 0; i < 19; i++){
+        for(let j=index+1; j < index+2; j++){
+           let tableCell =  document.getElementById("gameBoard").rows[i].cells[j];
+           if(i == 0){
+               tableCell.style.borderTop = "3px solid brown";
+           }
+           else if(i == 18){
+            tableCell.style.borderBottom = "3px solid brown";
+           }
+           if(tableCell.value == undefined && (i != 7 && i != 8 && i != 18)){
+                tableCell.style.backgroundColor = "beige";
+            }
+            tableCell.style.borderRight = "3px solid brown";
+            tableCell.style.borderLeft = "3px solid brown"
+        }
+    }
+}
+
+// Funktion där vi kollar vilken spelare som ska slå. 
+function checkTurn(){
+    let playerMaxIndex = playerArray.length;
+    let player = "player";
+
+
+    if(playerMaxIndex > 1){
+        playerMaxIndex -= 1;
+    }
+    numberOfThrows = 3;
+    for(let x = 0; x < 5; x ++){
+        document.getElementById(diceArray[x]).style.backgroundColor = "lightgreen";
+        document.getElementById(diceArray[x]).value = "X";
+        document.getElementById(diceArray[x]).removeAttribute('disabled');
+    }
+
+    if(playerArray[playerIndex+1] == undefined){
+        removeColor(startIndex);
+        playerIndex = 0;
+        startIndex = 0;
+        // document.getElementById("playersTurn").innerHTML = "Current player: " + playerArray[playerIndex];
+        document.getElementById("nrThrows").innerHTML = "Number of throws left: 3";
+        document.getElementById(player.concat(countPlayers)).style.border = "none";
+        document.getElementById(player.concat(playerIndex)).style.border = "3px solid black";
+        addColor(startIndex);
+        countPlayers = 0;
+    }
+    else if(playerIndex != playerMaxIndex){
+        removeColor(startIndex);
+        playerIndex += 1;
+        startIndex += 1;
+        // document.getElementById("playersTurn").innerHTML = "Current player: " + playerArray[playerIndex];
+        document.getElementById("nrThrows").innerHTML = "Number of throws left: 3";
+        document.getElementById(player.concat(countPlayers+1)).style.border = "3px solid black";
+        document.getElementById(player.concat(countPlayers)).style.border = "none";
+        addColor(startIndex);
+        countPlayers += 1;
+    }
+    // else if(playerIndex == playerMaxIndex){
+    //     playerIndex += 1;
+    //     startIndex += 1;
+    //     countPlayers = 0;
+    //     document.getElementById("nrThrows").innerHTML = "Number of throws left: 3";
+    //     document.getElementById(player.concat(countPlayers)).style.border = "3px solid black";
+    //     document.getElementById(player.concat(playerIndex)).style.border = "none";
+    //     console.log("här3");
+    // }  
 }
 
 // Funktion som kollar om ett värde finns flera gånger. 
@@ -286,10 +412,10 @@ function addValue(cell_id){
         added = true;
     }
     else if(document.getElementById(cell_id).value == undefined && cell_id.includes(tableSpots[startIndex]  + "_pair")){
-    added = checkCombination(cell_id,2);
+        added = checkCombination(cell_id,2);
     }
     else if(document.getElementById(cell_id).value == undefined &&cell_id.includes(tableSpots[startIndex]  + "_twoPair")){
-    added = checkCombination(cell_id,100);
+        added = checkCombination(cell_id,100);
     }
     else if(document.getElementById(cell_id).value == undefined && cell_id.includes(tableSpots[startIndex]  + "_triss")){
         added = checkCombination(cell_id,3);
@@ -359,7 +485,7 @@ function addValue(cell_id){
             }
             else if(oneSum < 63 && valueCheck == 6){
                 document.getElementById(sumIndex).innerHTML = 0;
-                document.getElementById(sumIndex).value = 0;
+                document.getElementById(sumIndex).value =  0;
             }
         }  
     }
@@ -394,35 +520,7 @@ function addValue(cell_id){
 
 
     if(added){
-        let playerMaxIndex = playerArray.length;
-        if(playerMaxIndex > 1){
-            playerMaxIndex -= 1;
-        }
-        numberOfThrows = 0;
-        for(let x = 0; x < 5; x ++){
-            document.getElementById(diceArray[x]).style.backgroundColor = "green";
-            document.getElementById(diceArray[x]).value = "X";
-            document.getElementById(diceArray[x]).removeAttribute('disabled');
-        }
-
-        if(playerArray[playerIndex+1] == undefined){
-            playerIndex = 0;
-            startIndex = 0;
-            document.getElementById("playersTurn").innerHTML = "Current player: " + playerArray[playerIndex];
-            document.getElementById("nrThrows").innerHTML = "You have made: 0 throw";
-        }
-        else if(playerIndex != playerMaxIndex){
-            playerIndex += 1;
-            startIndex += 1;
-            document.getElementById("playersTurn").innerHTML = "Current player: " + playerArray[playerIndex];
-            document.getElementById("nrThrows").innerHTML = "You have made: 0 throw";
-        }
-        else if(playerIndex == playerMaxIndex){
-            playerIndex += 1;
-            startIndex += 1;
-            document.getElementById("playersTurn").innerHTML = "Current player: " + playerArray[playerIndex];
-            document.getElementById("nrThrows").innerHTML = "You have made: 0 throw";
-        }  
+        checkTurn();
     }   
 }
 
@@ -434,10 +532,16 @@ function startGame(){
     document.getElementById("removePlayer").disabled = true;
     document.getElementById("start").style.backgroundColor = "grey";
     document.getElementById("start").disabled = true;
-    document.getElementById("playersTurn").innerHTML = "Current player: " + playerArray[playerIndex];
+    // document.getElementById("playersTurn").innerHTML = "Current player: " + playerArray[playerIndex];
     // document.getElementById("header-container").style.display = "block";
 }
 
+// Funktion där vi avslutar och börjar om matchen. 
+function endGame(){
+    location.reload();
+}
+
+// Chatbox 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
@@ -446,22 +550,25 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
-// Funktion där vi avslutar och börjar om matchen. 
-function endGame(){
-    location.reload();
-}
-
-
-
-
-// Chatbox 
+let myMessages = document.getElementById("myMsg");
+let textBubble = document.getElementById("bubble");
 let messages = document.getElementById("allMsg");
 let textbox = document.getElementById("userMsg");
 let submit = document.getElementById("submitMsg");
+let chatUsername = document.getElementById("chatUser");
 
 submit.addEventListener("click", function(){
     let newMessage = document.createElement("li");
+    // let img = document.createElement("img");
+    // img.src = "avatar.png";
+    // img.width=30;
+    // img.height=30;
+    // img.style.borderRadius = "50%"; 
+    // img.style.marginLeft = "10px";
+    // newMessage.style.marginLeft = "10px";
+
     newMessage.innerHTML = textbox.value;
-    messages.appendChild(newMessage);
+    textBubble.appendChild(newMessage);
+    myMessages.style.display = "block";
     textbox.value = "";
 }); 
